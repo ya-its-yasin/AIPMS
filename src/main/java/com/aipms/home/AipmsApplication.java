50db3dd51d7ba4452041892fac7ed85a2c6812eb
ya-its-yasin <yaitsyasin@gmail.com>
2024-02-15 09:42:19 +0000

    Running spring boot application

---
+++ src/main/java/com/aipms/home/AipmsApplication.java
@@ -0,0 +1,13 @@
+package com.aipms.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AipmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AipmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aipms/home/AipmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aipms.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AipmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
