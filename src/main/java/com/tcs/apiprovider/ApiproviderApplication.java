4c855c0405785adf04852803b9ca6c37e3c1549e
KaruppuRajaN <karuppuraja21@gmail.com>
2024-02-20 10:22:05 +0000

    This is API, Kindly Don't pull this

---
+++ src/main/java/com/tcs/apiprovider/ApiproviderApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.apiprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiproviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiproviderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcs/apiprovider/ApiproviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs.apiprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiproviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
