19816de4a131466040d8c0f6fedd218f0e2df06b
ya-its-yasin <yaitsyasin@gmail.com>
2024-02-29 06:26:31 +0000

    bonds updates

--- src/main/java/com/tcs/apiprovider/controller/ApiController.java
+++ src/main/java/com/tcs/apiprovider/controller/ApiController.java
@@ -1,12 +1,14 @@
 package com.tcs.apiprovider.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tcs.apiprovider.model.FloatingRateBonds;
 
+@CrossOrigin(origins = "http://localhost:4200") //port number of angular application
 @RestController
 @RequestMapping("/apiprovider")
 public class ApiController {
